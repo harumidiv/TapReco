c53b1a68111dd8607f660b5fd71f37d0cd270e4e
佐川晴海[YUMEMI]/Sagawa Harumi <harumidiv@yumemi.co.jp>
2021-08-30 20:28:34 +0900

    TapRecoのプロジェクトを作成しました

---
+++ TapReco/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TapReco
+//
+//  Created by 佐川 晴海 on 2021/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TapReco/TapRecoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapRecoApp.swift
+//  TapReco
+//
+//  Created by 佐川 晴海 on 2021/08/30.
+//
+
+import SwiftUI
+
+@main
+struct TapRecoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
