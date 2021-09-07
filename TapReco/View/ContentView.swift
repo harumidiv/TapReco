ba3dd43e441103da857d0550537e497a24d8c7bf
佐川晴海[YUMEMI]/Sagawa Harumi <harumidiv@yumemi.co.jp>
2021-09-07 20:38:42 +0900

    タップをした時に録音開始、レイアウト変更を行うようにしました

--- TapReco/View/ContentView.swift
+++ TapReco/View/ContentView.swift
@@ -14,29 +14,45 @@ struct ContentView: View {
     @State var isRecording: Bool = false
     
     var body: some View {
-        VStack {
-            if !isRecording {
-                Button("録画開始") {
-                    self.audioRecorder.record()
-                    isRecording = true
+        if !isRecording {
+            ZStack {
+                Rectangle()
+                    .frame(width: UIScreen.main.bounds.width,
+                           height: UIScreen.main.bounds.height)
+                    .foregroundColor(.orange)
+                    .onTapGesture {
+                        if !isRecording {
+                            self.audioRecorder.record()
+                            isRecording = true
+                        }
+                    }
+                
+                Image(systemName: "star")
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .background(Color.green)
+                    .onTapGesture { /*NOP*/ }
+                    .allowsTightening(false)
+                // TODO ここは半モーダルViewに移行する
+//                VStack {
+//                    Button("再生開始") {
+//                        self.audioPlayer.playStart()
+//                    }.padding(.bottom, 10.0)
+//                    Button("再生停止") {
+//                        self.audioPlayer.playStop()
+//                    }.padding(.bottom, 10.0)
+//                }
+            }
+        } else {
+            HStack {
+                Button("録画停止") {
+                    _ = self.audioRecorder.recordStop()
+                    isRecording = false
                 }.padding(.bottom, 10.0)
-            } else {
-                HStack {
-                    Button("録画停止") {
-                        _ = self.audioRecorder.recordStop()
-                        isRecording = false
-                    }.padding(.bottom, 10.0)
-                    MicrophoneVolumeView()
-                }
+                MicrophoneVolumeView()
             }
             
-            Button("再生開始") {
-                self.audioPlayer.playStart()
-            }.padding(.bottom, 10.0)
-            Button("再生停止") {
-                self.audioPlayer.playStop()
-            }.padding(.bottom, 10.0)
         }
+        
     }
 }
 
