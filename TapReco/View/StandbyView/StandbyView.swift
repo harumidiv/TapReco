31a2af4e579c7bb2dadb8d0a8af180d33ecb11a5
佐川晴海[YUMEMI]/Sagawa Harumi <harumidiv@yumemi.co.jp>
2021-12-15 00:47:47 +0900

    パーミッションが許可されていない場合の導線を作成しました

--- TapReco/View/StandbyView/StandbyView.swift
+++ TapReco/View/StandbyView/StandbyView.swift
@@ -11,6 +11,7 @@ import AVFoundation
 struct StandbyView: View {
     @Binding var isRecording: Bool
     @State var isPresentedRecordListView = false
+    @State var isPresentedAlertDialog = false
     @StateObject private var audioRecorder = AudioRecorderImpl()
     
     private let topMargin: CGFloat = 16
@@ -31,13 +32,23 @@ struct StandbyView: View {
                 impactHeavy.impactOccurred()
                 
                 if !isMicrophoneAuthorizationApproved() {
-                    // TODO ダイアログなどを表示してユーザに提示する必要がある、設定に飛ばすのが良い？
-                    print("ダイアログを出す")
+                    isPresentedAlertDialog = true
                     return
                 }
                 isRecording = true
             })
             .ignoresSafeArea()
+            .alert(isPresented: $isPresentedAlertDialog) {
+                Alert(
+                    title: Text("マイクへのアクセス許可がありません"),
+                    message: Text("[設定]に移動して、権限を許可してください"),
+                    primaryButton: .cancel(Text("キャンセル")),
+                    secondaryButton: .default(Text("設定"), action: {
+                        if let url = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) {
+                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                        }
+                    }))
+            }
         
         VStack(spacing: 0) {
             DotLineView()
