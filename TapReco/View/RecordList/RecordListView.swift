683e259a794407d168e5624acf30ebdc39933d59
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-01-19 18:13:52 +0900

    選択されたセルの形状を変形させるようにしました

--- TapReco/View/RecordList/RecordListView.swift
+++ TapReco/View/RecordList/RecordListView.swift
@@ -8,33 +8,79 @@
 import SwiftUI
 
 struct RecordListView: View {
+    struct ViewModel {
+        var recordList: [RecordData]
+    }
+    struct RecordData {
+        let title: String
+        let recordDate: String
+        let fileName:String
+        let fileSize: String
+        let fileLength: String
+        var isSelected: Bool = false
+    }
+    
+    @State private var viewModel: ViewModel?
+    
     @StateObject private var audioPlayer = AudioPlayerImpl()
     @Binding var isPresentedRecordListView: Bool
     var body: some View {
         ZStack {
-            VStack(spacing: 0) {
-                RecordListHeaderView(isPresentedRecordListView: $isPresentedRecordListView)
-                List {
-                    ForEach(0..<recordList().count) { index in
-                        Button(action: {
-                            audioPlayer.playStart(fileName: recordList()[index])
-                        }){
-                            RecordListCellView(viewModel: .init(title: recordList()[index]))
+            if let viewModel = viewModel {
+                VStack(spacing: 0) {
+                    RecordListHeaderView(isPresentedRecordListView: $isPresentedRecordListView)
+                    List {
+                        
+                        ForEach(0..<viewModel.recordList.count) { index in
+                            if viewModel.recordList[index].isSelected {
+                                Button(action: {
+                                    print("record on Play")
+                                    audioPlayer.playStart(fileName: viewModel.recordList[index].fileName)
+                                }){
+                                    Text("選択されてる状態")
+                                }
+                                
+                            } else {
+                                Button(action: {
+                                    // すでに選択済みのセルを元の状態に戻す
+                                    viewModel.recordList.indices.forEach { self.viewModel?.recordList[$0].isSelected = false }
+                                    self.viewModel?.recordList[index].isSelected = true
+                                }){
+                                    RecordListCellView(viewModel: .init(title: viewModel.recordList[index].title))
+                                }
+                            }
                         }
                     }
+                    .listStyle(PlainListStyle())
                 }
-                .listStyle(PlainListStyle())
+            } else {
+                // TODO インジケータと読み込み中の形のレイアウトを作成する
+                Text("loading中")
             }
+        }
+        .onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                fetchRecordList()
+            }
+            print("onAppear")
         }
     }
     
-    private func recordList() -> [String] {
+    private func fetchRecordList() {
         let documentPath = NSHomeDirectory() + "/Documents"
         guard let fileNames = try? FileManager.default.contentsOfDirectory(atPath: documentPath) else {
-            return []
+            return
         }
         
-        return fileNames
+        let recordList: [RecordData] = fileNames.compactMap{
+            return RecordData(title: $0,
+                              recordDate: $0,
+                              fileName: $0,
+                              fileSize: "3.5MB",
+                              fileLength: "03:05")
+        }
+        
+        self.viewModel = .init(recordList: recordList)
     }
 }
 
