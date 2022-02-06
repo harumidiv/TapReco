541eec5fc7d088968a8b2ef221dd7956fd378fec
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-02-06 22:17:38 +0900

    タップ領域を修正しました

--- TapReco/View/Standby/StandbyView.swift
+++ TapReco/View/Standby/StandbyView.swift
@@ -21,55 +21,58 @@ struct StandbyView: View {
         let dotLineHeight = UIScreen.main.bounds.height - (topMargin + bottomMargin)
         
         ZStack {
+            // 背景色用のView
             Rectangle()
-                .frame(width: UIScreen.main.bounds.width,
-                       height: UIScreen.main.bounds.height)
                 .foregroundColor(Color("tp_gray"))
-                .onTapGesture {
-                    let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
-                    impactHeavy.prepare()
-                    impactHeavy.impactOccurred()
-                    
-                    if !presenter.isAutholized {
-                        presenter.apply(inputs: .didTapRecording)
-                        return
-                    }
-                    
-                    isRecording = true
-                }
                 .ignoresSafeArea()
-                .alert(isPresented: $presenter.isShowAlertDialog, content: presenter.alertBuilder)
             
             ZStack {
-                // TODO 画像を差し替える
-                Image("wakusen")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: dotLineWidth,
-                           height: dotLineHeight,
-                           alignment: .center)
-                VStack(spacing: 18) {
-                    Image("icon_microphone")
-                    Text("タップして録音開始")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                }
-            }
-            .onTapGesture {}
-            .allowsHitTesting(false)
-            
-            GeometryReader { geometry in
-                let buttonHeight: CGFloat = 86
-                let buttonWidth: CGFloat = 205
-                let bottomMargin: CGFloat = 74
-                SlideUPActionView(isPresentedRecordListView: $presenter.isShowRecordList)
-                    .frame(width: buttonWidth, height: buttonHeight)
-                    .position(x: geometry.size.width / 2,
-                              y: geometry.size.height - (bottomMargin + buttonHeight / 2))
-                    .fullScreenCover(isPresented: $presenter.isShowRecordList) {
-                        RecordListView(isPresentedRecordListView: $presenter.isShowRecordList)
+                // タップ領域用のView
+                Rectangle()
+                    .foregroundColor(Color("tp_gray"))
+                    .onTapGesture {
+                        let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
+                        impactHeavy.prepare()
+                        impactHeavy.impactOccurred()
+                        
+                        if !presenter.isAutholized {
+                            presenter.apply(inputs: .didTapRecording)
+                            return
+                        }
+                        
+                        isRecording = true
                     }
+                    .alert(isPresented: $presenter.isShowAlertDialog, content: presenter.alertBuilder)
+                ZStack {
+                    Image("wakusen")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: dotLineWidth,
+                               height: dotLineHeight,
+                               alignment: .center)
+                    VStack(spacing: 18) {
+                        Image("icon_microphone")
+                        Text("タップして録音開始")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    }
+                }
+                .onTapGesture {}
+                .allowsHitTesting(false)
+                
+                GeometryReader { geometry in
+                    let buttonHeight: CGFloat = 86
+                    let buttonWidth: CGFloat = 205
+                    let bottomMargin: CGFloat = 74
+                    SlideUPActionView(isPresentedRecordListView: $presenter.isShowRecordList)
+                        .frame(width: buttonWidth, height: buttonHeight)
+                        .position(x: geometry.size.width / 2,
+                                  y: geometry.size.height - (bottomMargin + buttonHeight / 2))
+                        .fullScreenCover(isPresented: $presenter.isShowRecordList) {
+                            RecordListView(isPresentedRecordListView: $presenter.isShowRecordList)
+                        }
+                }
             }
         }
     }
