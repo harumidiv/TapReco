81f7e3f97e823a0792c3b7d33fb0c09777d20a73
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-07-27 17:16:52 +0900

    Realmに保存する値を取得できるようにしました

--- TapReco/Utility/AudioRecorder.swift
+++ TapReco/Utility/AudioRecorder.swift
@@ -14,11 +14,11 @@ protocol AudioRecoder: AVAudioRecorderDelegate, ObservableObject {
  
 final class AudioRecorderImpl: NSObject {
     private var audioRecorder: AVAudioRecorder!
-    
-    private func createURL() -> URL{
+    private var currentRecordingTitle: String?
+
+    private func createURL(title: String) -> URL{
         let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        let recordTitle: String = Date().toString(format: .tapRecorYear) + ".m4a"
-        let filePath = documentPath.appendingPathComponent(recordTitle)
+        let filePath = documentPath.appendingPathComponent(title)
 
         return filePath
     }
@@ -38,13 +38,54 @@ extension AudioRecorderImpl: AudioRecoder {
             AVSampleRateKey: 44100.0
         ]
         
-        audioRecorder = try! AVAudioRecorder(url: createURL(), settings: settings)
+        currentRecordingTitle = Date().toString(format: .tapRecorYear) + ".m4a"
+        audioRecorder = try! AVAudioRecorder(url: createURL(title: currentRecordingTitle!), settings: settings)
         audioRecorder.prepareToRecord()
         audioRecorder.record()
     }
     
     func recordStop()  {
         audioRecorder.stop()
+        
+        let filePath = NSHomeDirectory() + "/Documents/" + currentRecordingTitle!
+        
+        let fileSize: String = getFileSize(filePath: filePath)
+        let playbackTime: String = getPlaybackTime(filePath: filePath)
+        let recordingDate: String = currentRecordingTitle!.components(separatedBy: "+").first!
+        
+        
+        print("date: \(recordingDate), fileSize: \(fileSize), playbackTime: \(playbackTime)")
+        
+        // TODO Realmに必要な値を保存する
     }
         
+}
+
+// MARK: - Private Method
+private extension AudioRecorderImpl {
+    func getFileSize(filePath: String) -> String {
+        let fileAttributes = try! FileManager.default.attributesOfItem(atPath: filePath)
+
+        if let bytes = fileAttributes[.size] as? Int64 {
+            let bcf = ByteCountFormatter()
+            bcf.allowedUnits = [.useKB, .useMB, .useBytes, .useAll]
+            bcf.countStyle = .file
+            return bcf.string(fromByteCount: bytes)
+        }
+        return ""
+    }
+    
+    func getPlaybackTime(filePath: String) -> String {
+        let audioUrl = URL(fileURLWithPath: filePath)
+        let audioPlayer: AVAudioPlayer = try! AVAudioPlayer(contentsOf: audioUrl)
+        
+        let duration = Int(audioPlayer.duration)
+        let min = duration / 60
+        let sec = duration % 60
+        
+        let minString = min >= 10 ? min.description : "0" + min.description
+        let secString = sec >= 10 ? sec.description : "0" + sec.description
+        
+        return min > 60 ? "1時間以上" : minString + ":" + secString
+    }
 }
