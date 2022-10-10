fc123848d273bbce46577846a75134d9b18ea4a5
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-10 19:25:19 +0900

    新規録音

--- TapReco/Utility/Model/RecordingData.swift
+++ TapReco/Utility/Model/RecordingData.swift
@@ -46,8 +46,8 @@ struct RecordData: Identifiable, Codable {
 extension RecordData {
     static let sampleData: [RecordData] =
     [
-    RecordData(title: "My録音1", recordDate:  "2022.10.07_14:54", fileName: "マイ録音", fileSize: "3.5MB", fileLength: "3:15"),
-    RecordData(title: "My録音2", recordDate:  "2022.10.07_14:54", fileName: "マイ録音", fileSize: "3.5MB", fileLength: "3:15"),
-    RecordData(title: "My録音3", recordDate:  "2022.10.07_14:54", fileName: "マイ録音", fileSize: "3.5MB", fileLength: "3:15")
+    RecordData(title: "新規録音", recordDate:  "2022.10.07_14:54", fileName: "マイ録音", fileSize: "3.5MB", fileLength: "3:15"),
+    RecordData(title: "新規録音", recordDate:  "2022.10.07_14:54", fileName: "マイ録音", fileSize: "3.5MB", fileLength: "3:15"),
+    RecordData(title: "新規録音", recordDate:  "2022.10.07_14:54", fileName: "マイ録音", fileSize: "3.5MB", fileLength: "3:15")
     ]
 }
