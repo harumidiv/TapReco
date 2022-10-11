dd9fb95428a7c8eec6552a09e4d4874cee4ee4f6
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-11 09:23:14 +0900

    画像を追加しました

---
+++ TapReco/View/RecordList/Component/SelectView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectView.swift
+//  TapReco
+//
+//  Created by 佐川 晴海 on 2022/10/11.
+//
+
+import SwiftUI
+
+struct SelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectView()
+    }
+}
