6eb721a8f0c7ebca9575c46a101c66b073f417fe
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-12 22:34:24 +0900

    クラッシュするバグを修正しました

--- TapReco/View/RecordList/RecordListView.swift
+++ TapReco/View/RecordList/RecordListView.swift
@@ -59,14 +59,6 @@ struct RecordListView: View {
         return sortRrcord.filter{ $0.title.contains(searchText)}
     }
 
-    private var selectedIndex: Int {
-        records.firstIndex(where: { $0.isSelected })!
-    }
-
-    private var playRecord: RecordData {
-        return records[selectedIndex]
-    }
-
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
@@ -92,7 +84,7 @@ struct RecordListView: View {
                         } else {
                             Button(action: {
                                 setSelectedState(selectRecord: record)
-                                audioPlayer.setup(fileName: playRecord.fileName)
+                                audioPlayer.setup(fileName: record.fileName)
                             }){
                                 RecordListCardView(record: record,
                                                    backgroundColor: AppColor.boxGray,
