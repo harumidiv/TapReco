1b0752ff32f13e43f8d915ab697747e49d1c223a
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-13 10:26:05 +0900

    リスト画面にfilter後のデータをStateで保持するように変更しました

--- TapReco/View/RecordList/RecordListView.swift
+++ TapReco/View/RecordList/RecordListView.swift
@@ -19,62 +19,23 @@ struct RecordListView: View {
     @State private var searchText: String = ""
     @State private var sortType: SortType = .dateNew
     @State private var isShowSortView: Bool = false
-    
-    private var displyRecords: [RecordData] {
-        func stringToInt(text: String) -> Int64 {
-            let splitNumber = (text.components(separatedBy: NSCharacterSet.decimalDigits.inverted))
-            return Int64(splitNumber.joined())!
-        }
-        
-        let sortRrcord: [RecordData]
-        switch sortType {
-        case .dateNew:
-            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
-                return stringToInt(text: lRecord.recordDate) > stringToInt(text: rRecord.recordDate)
-            })
-        case .dateOld:
-            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
-                return stringToInt(text: lRecord.recordDate) < stringToInt(text: rRecord.recordDate)
-            })
-        case .recordTimeLong:
-            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
-                return stringToInt(text: lRecord.recordTime) > stringToInt(text: rRecord.recordTime)
-            })
-        case .recordTimeShort:
-            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
-                return stringToInt(text: lRecord.recordTime) < stringToInt(text: rRecord.recordTime)
-            })
-            
-        case .fileSizeLarge:
-            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
-                return stringToInt(text: lRecord.fileSize) > stringToInt(text: rRecord.fileSize)
-            })
-            
-        case .fileSizeSmall:
-            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
-                return stringToInt(text: lRecord.fileSize) < stringToInt(text: rRecord.fileSize)
-            })
-        }
-        
-        if searchText.isEmpty { return sortRrcord }
-        return sortRrcord.filter{ $0.title.contains(searchText)}
-    }
-    
+
+    @State private var displayRecords: [RecordData] = []
+
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
                 RecordListHeaderView(isShowRecordList: $isShowRecordList,
                                      isShowSortView: $isShowSortView,
-                                     displyRecords: displyRecords,
+                                     displayRecords: displayRecords,
                                      searchText: $searchText) {
-                    if records.contains(where: { $0.isSelected == true }) {
-                        resetSelectedState()
+                    if displayRecords.contains(where: { $0.isSelected == true }) {
                         audioPlayer.playStop()
                     }
                     isShowRecordList = false
                 }
                 List {
-                    ForEach(displyRecords) { record in
+                    ForEach(displayRecords) { record in
                         if record.isSelected {
                             RecordListCardView(record: record,
                                                backgroundColor: AppColor.boxBlack,
@@ -104,13 +65,19 @@ struct RecordListView: View {
             }
             .blur(radius: isShowSortView ? 2.0 : 0.0)
             
-            if records.contains(where: { $0.isSelected == true }) {
+            if displayRecords.contains(where: { $0.isSelected == true }) {
                 VStack(spacing: 0) {
                     Spacer()
                     RecordListPlayerView(saveAction: saveAction,
                                          isPlaying: $isPlaying,
-                                         records: $records,
-                                         audioPlayer: audioPlayer)
+                                         record: $displayRecords.filter{$0.isSelected.wrappedValue == true}.first!,
+                                         audioPlayer: audioPlayer){record in
+                        audioPlayer.playStop()
+                        let selectedIndex = records.firstIndex(where: { $0.id == record.id })!
+                        // 大元のデータを削除してデータ更新をかける
+                        records.remove(at: selectedIndex)
+                        saveAction()
+                    }
                 }
                 .ignoresSafeArea(edges: [.top])
                 .blur(radius: isShowSortView ? 2.0 : 0.0)
@@ -121,10 +88,53 @@ struct RecordListView: View {
             }
         }
         .background(AppColor.background)
+        .onAppear{
+            displayRecords = getDisplayRecord()
+        }
     }
 }
 
 private extension RecordListView {
+    func getDisplayRecord() -> [RecordData] {
+        func stringToInt(text: String) -> Int64 {
+            let splitNumber = (text.components(separatedBy: NSCharacterSet.decimalDigits.inverted))
+            return Int64(splitNumber.joined())!
+        }
+
+        let sortRrcord: [RecordData]
+        switch sortType {
+        case .dateNew:
+            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
+                return stringToInt(text: lRecord.recordDate) > stringToInt(text: rRecord.recordDate)
+            })
+        case .dateOld:
+            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
+                return stringToInt(text: lRecord.recordDate) < stringToInt(text: rRecord.recordDate)
+            })
+        case .recordTimeLong:
+            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
+                return stringToInt(text: lRecord.recordTime) > stringToInt(text: rRecord.recordTime)
+            })
+        case .recordTimeShort:
+            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
+                return stringToInt(text: lRecord.recordTime) < stringToInt(text: rRecord.recordTime)
+            })
+
+        case .fileSizeLarge:
+            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
+                return stringToInt(text: lRecord.fileSize) > stringToInt(text: rRecord.fileSize)
+            })
+
+        case .fileSizeSmall:
+            sortRrcord = records.sorted(by: { lRecord, rRecord -> Bool in
+                return stringToInt(text: lRecord.fileSize) < stringToInt(text: rRecord.fileSize)
+            })
+        }
+
+        if searchText.isEmpty { return sortRrcord }
+        return sortRrcord.filter{ $0.title.contains(searchText)}
+    }
+
     func updateTitle(title: String, id: UUID) {
         self.records = records.compactMap{
             if $0.id != id { return $0 }
@@ -135,15 +145,9 @@ private extension RecordListView {
     
     // 対象のセルを選択状態にする
     func setSelectedState(selectRecord: RecordData) {
-        self.records = records.compactMap{
+        self.displayRecords = displayRecords.compactMap{
             let isSelected = $0.id == selectRecord.id
             return RecordData(record: $0, isSelected: isSelected)
-        }
-    }
-    
-    func resetSelectedState()  {
-        self.records = records.compactMap{
-            return RecordData(record: $0, isSelected: false)
         }
     }
 }
