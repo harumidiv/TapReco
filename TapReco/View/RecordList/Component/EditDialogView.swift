ac62e77fbcf348a5a1c3f5569a7e655980896b75
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-10-14 11:18:22 +0900

    EditDialogViewを修正しました

--- TapReco/View/RecordList/Component/EditDialogView.swift
+++ TapReco/View/RecordList/Component/EditDialogView.swift
@@ -8,27 +8,38 @@
 import SwiftUI
 
 struct EditDialogView: View {
+    @Binding var isShowEditAlert: Bool
+    let placeholderText: String
+    var updateAction: ()->Void
+
     @State private var editText: String = ""
     var body: some View {
         ZStack {
-            Color.red
+            Rectangle()
+                .foregroundColor(.black)
+                .opacity(0.2)
             VStack {
                 Text("タイトル編集")
                     .font(Font.system(size: 16, weight: .bold))
                     .padding()
-                TextField("プレースホルダー", text: $editText)
+                TextField(placeholderText, text: $editText)
                     .background(AppColor.sortBackground)
                     .padding(.horizontal)
 
                 HStack {
                     Button("キャンセル"){
-                        // TODO キャンセル処理
+                        isShowEditAlert.toggle()
                     }
-                        .frame(width: 150)
+                    .frame(width: 150)
                     Button("決定"){
-                        // TODO 保存処理
+                        if editText.isEmpty {
+                            isShowEditAlert.toggle()
+                        } else {
+                            updateAction()
+                            isShowEditAlert.toggle()
+                        }
                     }
-                        .frame(width: 150)
+                    .frame(width: 150)
                 }
                 .padding()
             }
@@ -42,6 +53,7 @@ struct EditDialogView: View {
 
 struct EditDialogView_Previews: PreviewProvider {
     static var previews: some View {
-        EditDialogView()
+        EditDialogView(isShowEditAlert: .constant(true),
+                       placeholderText: "プレースホルダー"){}
     }
 }
